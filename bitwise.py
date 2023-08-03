148293379b616d3f7d2b487321e120c6c7bd67ed
promethesus <jeremyandrews707@gmail.com>
2023-08-03 05:56:54 -0700

    update directory for organization

--- bitwise.py
+++
@@ -1,7 +0,0 @@
-import timeit
-
-print("Bitwise method time:",
-      timeit.timeit("for x in range(100): x & 1 == 0", number=100000))
-
-print("Modulus method time:",
-      timeit.timeit("for x in range(100): x % 2 == 0", number=100000))
